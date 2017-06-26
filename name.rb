803ee74bb8ea5aab0038e11ebea6e7da51be3c3c
David Zheng <davidzheng@knights.ucf.edu>
2017-06-26 17:14:45 -0400

    fixed logo and home page

--- name.rb
+++ name.rb
@@ -1,5 +1,7 @@
 # name.rb
 require 'sinatra'
+require 'sendgrid-ruby'
+include SendGrid
 
 get '/' do
   erb :home
@@ -11,6 +13,23 @@ end
 
 get '/contact' do
   erb :contact
+end
+
+post '/contact' do
+  # get from address from form
+  # to do: verify email is valid
+  from = Email.new(email: params[:email])
+  to = Email.new(email: 'zhengdavid94@gmail.com')
+  subject = params[:subject]
+  content = Content.new(type: 'text/plain', value: params[:body])
+  mail = Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+  puts response.headers
+  redirect '/contact'
 end
 
 get '/faq' do
